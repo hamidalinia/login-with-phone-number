7cdae0719328305a879127081263bf461e0878ef
Hamid Alinia <a.goodlookingboy@gmail.com>
2026-06-24 12:57:10 +0330

    update to new version, add new gateways

--- login-with-phonenumber.php
+++ login-with-phonenumber.php
@@ -24,7 +24,7 @@ require_once plugin_dir_path(__FILE__) . 'inc/helper-functions.php';
 require_once plugin_dir_path(__FILE__) . 'gateways/class-lwp-custom-api.php';
 require_once plugin_dir_path(__FILE__) . 'gateways/lwp-drpayamak/lwp-drpayamak.php';
 require_once plugin_dir_path(__FILE__) . 'gateways/lwp-kavenegar/lwp-kavenegar.php';
-require_once plugin_dir_path(__FILE__) . 'gateways/lwp-twilio/lwp-twilio.php';
+//require_once plugin_dir_path(__FILE__) . 'gateways/lwp-twilio/lwp-twilio.php';
 require_once plugin_dir_path(__FILE__) . 'gateways/lwp-netgsm/lwp-netgsm.php';
 
 
